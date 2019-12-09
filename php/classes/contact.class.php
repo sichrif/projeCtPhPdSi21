17f2888372029925f7d54ffad15e447d0da196d9
tahersaid <amosiwar@gmail.com>
2019-12-09 05:16:17 +0300

    dashbord admin

---
+++ php/classes/contact.class.php
@@ -0,0 +1,99 @@
+<?php
+
+require 'dbconnect.class.php';
+ 
+class message{
+    private $cnx;
+
+
+    public function __construct(){
+        $db=new BaseDonne;
+        $this->cnx= $db->connect();
+
+
+    }
+    public function num_message(){
+        try{
+            $req='SELECT * FROM message';
+            $result = $this->cnx->prepare($req);
+            $result->execute();
+            return $result;
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+    }
+    }
+
+    public function readAllmessage(){
+        try{
+            $req='SELECT A.cid,B.name,B.email,A.subject,A.msg
+             FROM message A,customer B where A.cid=B.cid';
+            $result = $this->cnx->prepare($req);
+            $result->execute();
+            return $result;
+
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+    }
+}
+/*public function add_new_vehicule($statu,$vnumber)
+{
+        try{
+            if(isset($_POST['done'])){   
+                $repp='INSERT INTO `vehicle`(`vid`, `status`, `vehicle_number`) 
+                VALUES (:vid,:statu,:vmumber)';              
+    $rep=$this->cnx->prepare($repp);
+    $rep->bindParam(':vid',$vid);
+    $rep->bindParam(':statu',$statu);
+    $rep->bindParam(':vmumber',$vnumber);
+    $rep->execute();
+
+ return $rep;
+              }
+}catch (Exception $e) {
+    echo  $e->getMessage();
+     }
+}
+
+public function delete($id){
+    try {
+        $repp='DELETE FROM vehicle WHERE vid =:param_pid';
+        $rep=$this->cnx->prepare($repp);
+        $rep->bindParam(':param_pid',$_GET['vid']);
+        $rep->execute();
+        return $rep;
+    
+    }catch (Exception $e) {
+        echo  $e->getMessage();
+         }
+}
+
+/*public function deleteAll(){
+    try{
+        $repp='DELETE FROM product ';
+        $rep=$this->cnx->prepare($repp);
+        $rep->execute();
+        return $rep;
+    }catch (Exception $e) {
+        echo  $e->getMessage();
+         }
+}
+
+public function update($vid,$statu,$v_number){
+    try {
+        $rep='UPDATE `vehicle` SET 
+        `vid`=:vid,`status`=:statu,`vehicle_number`=:v_number WHERE `vid`=:vid';
+        $rep=$this->cnx->prepare($rep);
+        $rep->bindParam(':vid',$vid);
+        $rep->bindParam(':statu',$statu);
+        $rep->bindParam(':v_number',$v_number);
+        $rep->execute();
+        return $rep;
+    
+    }catch (Exception $e) {
+        echo  $e->getMessage();
+         }
+}*/
+}
+
+?>
---
+++ php/store_employee.php
@@ -0,0 +1,10 @@
+<?php
+ include 'classes/vehicule.class.php';
+$vnumber=$_POST['v_number'];
+$status=$_POST['statu'];
+
+$aa=new vehicule;
+$bb=$aa->add_new_vehicule($vnumber,$status);
+header('location:../vehicle.php');
+
+ 
